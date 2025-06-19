b8f23bb5a51d2b235b02354e5b571f40d3d847e1
Zuzanna <zuzannaorzechowska4@gmail.com>
2025-06-19 19:55:34 +0200

    finished styling main component for unregistered user - without responsive (soon)

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@
     <AppHeader />
     <v-main id="main">
       <router-view>
-        <img src="./assets/logo_text.png" alt="SearchW">
+        <div class="wrapper">
+          <img src="./assets/logo_text.png" alt="SearchW" id="logo-text">
+          <div class="q-wrapper">
+            <q>The smart, fun way to test your mind and spot every hidden word.</q>
+          </div>
+          <div class="btns">
+            <button type="button" id="sign-in">Sign in</button>
+            <button type="button" id="random">Random</button>
+          </div>
+        </div>
       </router-view>
     </v-main>
     <AppFooter />
@@ -39,19 +48,60 @@ export default {
       min-height: 100vh;
       display: grid;
       grid-template-rows: auto 1fr auto;
-      background-image: url(./assets/background_drawing.png);
+      background-image: url(./assets/lines.png);
       background-repeat: no-repeat;
       background-position: center;
-      background-size: 85%;
+      background-size: contain;
       
       #main {
         display: flex;
         align-items: center;
         justify-content: center;
-        img {
-          width: 140%;
-          margin-bottom: 32%;
+        flex-direction: column;
+
+        .wrapper {
+          margin-top: 24px;
+          #logo-text {
+            width: 420px;
+            margin-left: 52px;
+          }
+  
+          .q-wrapper {
+            width: 408px;
+            text-align: center;
+            margin-left: 52px;
+            q {
+              font-size: 20px;
+            }
+          }
+  
+          .btns {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            gap: 20px;
+            margin-top: 24px;
+            margin-left: 52px;
+  
+            button{
+              font-size: 24px;
+              padding: 1% 6%;
+              border: 2px black solid;
+              border-radius: 6px;
+              font-weight: 500;
+            }
+  
+            #sign-in {
+              background-color: #3291C3;
+            }
+  
+            #random {
+              background-color: #f9f9f9;
+            }
+          }
         }
+
       }
     }
   }
