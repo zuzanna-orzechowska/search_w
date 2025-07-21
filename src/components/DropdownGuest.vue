9c5823be42a4171bd1c72ef7c560836b5ee2b07f
Zuzanna <zuzannaorzechowska4@gmail.com>
2025-07-21 21:39:04 +0200

    started working on dropdowns for user icon in header, changed folders - now it's views and components

---
+++ src/components/DropdownGuest.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-if="dropdownActive" class="dropdown-content">
+        <div class="dropdown-section">
+            <p>Come back to word searching’s journey</p>
+            <button class="dropdown-btn" @click="toLogin">Sign in</button>
+        </div>
+        <div class="dropdown-section">
+            <p>Don’t have an account?</p>
+            <button class="dropdown-btn white" @click="toRegister">Sign up</button>
+            <div class="otherRegister">
+                <p>or</p>
+                <div class="linksRegister">
+                    <img src="../assets/google-icon.svg" alt="Google icon">
+                    <img src="../assets/apple-icon.svg" alt="Apple icon">
+                    <img src="../assets/microsoft-icon.svg" alt="Microsoft icon">    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
