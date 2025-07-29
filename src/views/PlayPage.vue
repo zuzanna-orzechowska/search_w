fd8645b59ea2ae5e422e25a865774fb56b5ea565
Zuzanna <zuzannaorzechowska4@gmail.com>
2025-07-29 22:35:46 +0200

    added categories and changed text in Play

--- src/views/PlayPage.vue
+++ src/views/PlayPage.vue
@@ -1,7 +1,73 @@
 <template>
     <div class="container">
         <div class="wrapper">
-            <h1>Siema elo trzy dwa zero</h1>
+            <div class="text-container">
+                <h2>Play</h2>
+                <p class="bigger">Choose a category</p>
+                <p class="smaller">0/16 completed</p>
+            </div>
+            <div class="categories-container">
+                <div class="category">
+                    <img src="../assets/categories/fruit.png" alt="Fruit category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/animals.png" alt="Animals category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/body.png" alt="Body category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/emotions.png" alt="Emotions category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/cities.png" alt="Cities category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/countries.png" alt="Countries category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/movies.png" alt="Movie category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/actors.png" alt="Actors category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/fairytales.png" alt="Fairytales category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/games.png" alt="Games category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/electronics.png" alt="Electronics category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/food.png" alt="Food category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/plants.png" alt="Plants category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/space.png" alt="Space category">
+                    <button>Play</button>
+                </div>
+                <div class="category">
+                    <img src="../assets/categories/summer.png" alt="Summer category">
+                    <button>Play</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -26,10 +92,72 @@ body {
     .wrapper {
         background-color: rgba(174, 210, 229,0.5);
         backdrop-filter: blur(20px);
-        width: 600px;
-        height: 700px;
+        width: 60vw;
+        height: 90vh;
         border-radius: 6px;
         box-shadow:  4px 4px 10px 3px rgba(0,0,0,0.3);
+        display: flex;
+        align-items: center;
+        //justify-content: center;
+        flex-direction: column;
+    }
+
+    .text-container {
+        text-align: center;
+
+        h2 {
+            font-size: 64px;
+            font-weight: 500;
+            margin-bottom: 4px;
+        }
+
+        .bigger{
+            font-size: 32px;
+            margin-bottom: 4px;
+        }
+
+        .smaller {
+            font-size: 24px;
+        }
+    }
+
+    .categories-container {
+        display: grid;
+        grid-template-columns: repeat(5,1fr);
+        gap: 2vw;
+        margin-top: 42px;
+
+        .category {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 1vw;
+
+            img {
+                width: 136px;
+                border: 2px solid black;
+                border-radius: 6px;
+            }
+
+            button {
+                font-size: 20px;
+                padding: 1% 6%;
+                font-weight: 500;
+                background-color: #2A8DC1;
+                border: 2px solid black;
+                border-radius: 12px;
+                cursor: pointer;
+                transform: perspective(1px) translateZ(0);
+                box-shadow: 0 0 1px transparent;
+                transition-duration: 0.3s;
+                transition-property: box-shadow, transform;
+            }
+
+            button:hover {
+                box-shadow: 0px 8px 30px -4px rgba(8, 73, 111, 0.86);
+                transform: scale(1.1);
+            }
+        }
     }
 }
 
